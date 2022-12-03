a15983f668e4906ab017816595ba6079589d8a32
ZhangHao6810 <119721528+ZhangHao6810@users.noreply.github.com>
2022-12-03 20:50:40 +0800

    Update Hello.java
    2th commit

--- src/main/java/com/git/git_test/Hello.java
+++ src/main/java/com/git/git_test/Hello.java
@@ -3,5 +3,6 @@ package com.git.git_test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("git11111");
+        System.out.println("git22222");
     }
 }
